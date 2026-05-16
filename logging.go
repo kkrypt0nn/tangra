1c0a7e4af7f440b23bab66d82b5466b10a077803
Krypton <root@krypton.ninja>
2026-05-16 22:09:45 +0200

    fix: make fatal logs exit

--- logging.go
+++ logging.go
@@ -31,7 +31,13 @@ func (l Logger) Warn(msg string)                   { l.log(WARN, msg) }
 func (l Logger) Warnf(format string, args ...any)  { l.log(WARN, fmt.Sprintf(format, args...)) }
 func (l Logger) Error(msg string)                  { l.log(ERROR, msg) }
 func (l Logger) Errorf(format string, args ...any) { l.log(ERROR, fmt.Sprintf(format, args...)) }
-func (l Logger) Fatal(msg string)                  { l.log(FATAL, msg) }
-func (l Logger) Fatalf(format string, args ...any) { l.log(FATAL, fmt.Sprintf(format, args...)) }
+func (l Logger) Fatal(msg string) {
+	l.log(FATAL, msg)
+	os.Exit(1)
+}
+func (l Logger) Fatalf(format string, args ...any) {
+	l.log(FATAL, fmt.Sprintf(format, args...))
+	os.Exit(1)
+}
 func (l Logger) Trace(msg string)                  { l.log(TRACE, msg) }
 func (l Logger) Tracef(format string, args ...any) { l.log(TRACE, fmt.Sprintf(format, args...)) }
